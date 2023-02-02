cf65aebf161074bb40989c58560c31030a6f5d64
masiyematome <matomemasiye7@gmail.com>
2023-02-02 13:20:07 +0200

    I added code that configures the browser of our choice

---
+++ OrangeHrmDemo/OrangeHrmDemo/Web/Utilities/WebBrowserHelper.cs
@@ -0,0 +1,83 @@
+﻿
+
+using OpenQA.Selenium;
+using
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Edge;
+
+namespace OrangeHrmDemo.Web.Utilities
+{
+    public class WebBrowserHelper
+    {
+
+        public static IWebDriver SetUpBrowser(string browserName)
+        {
+
+            IWebDriver driver = null;
+
+            switch (browserName.ToLower())
+            {
+                
+                case "chrome":
+
+                    new DriverManager().SetUpDriver(new ChromeConfig());
+
+                    ChromeOptions chromeOptions = new ChromeOptions();
+
+                    chromeOptions.AcceptInsecureCertificates = true;
+
+                    driver = new ChromeDriver(chromeOptions);
+
+                    break;
+
+                case "firefox":
+
+                    new DriverManager().SetUpDriver(new FirefoxConfig());
+
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+                    firefoxOptions.AcceptInsecureCertificates = true;
+
+                    driver = new FirefoxDriver(firefoxOptions);
+
+                    break;
+
+                case "ie":
+
+                    new DriverManager().SetUpDriver(new InternetExplorerConfig());
+
+                    InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
+
+                    internetExplorerOptions.AcceptInsecureCertificates = true;
+
+                    driver = new InternetExplorerDriver(internetExplorerOptions);
+
+
+                    break;
+
+                case "edge":
+
+                    new DriverManager().SetUpDriver(new EdgeConfig());
+
+                    EdgeOptions edgeOptions = new EdgeOptions();
+
+                    edgeOptions.AcceptInsecureCertificates = true;
+
+                    driver = new EdgeDriver(edgeOptions);
+
+                    break;
+
+            }
+
+            driver.Manage().Window.Maximize();
+
+            return driver;
+
+        }
+
+    }
+}
